d0f257fc7a5bc762f1cf2ac175a503085e049dab
Landon George <landon.robert.george@gmail.com>
2022-08-30 09:36:01 -0500

    Add cats route with URL parameter

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -14,6 +14,12 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>About</h1>")
 }
 
+func catHandler(w http.ResponseWriter, r *http.Request) {
+	catID := chi.URLParam(r, "catID")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "You are looking at cat %v", catID)
+}
+
 func errorHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Page not found", http.StatusNotFound)
 }
@@ -22,6 +28,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
 	r.Get("/about", aboutHandler)
+	r.Get("/cats/{catID}", catHandler)
 	r.NotFound(errorHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
